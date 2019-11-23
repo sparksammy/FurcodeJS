2293769ed060b1d28572bd462c4f1aba3579d532
Samuel Lord <sllord@icloud.com>
2019-11-22 19:56:10 -0600

    Create furcode.js

---
+++ furcode.js
@@ -0,0 +1,17 @@
+//credit to clarmond begin
+
+function fureval(code) {
+  if code.includes("owo") {
+    var owoText = code.replace("owo ","");
+    console.log("owoText")
+  }
+  if code.includes("uwu") {
+    var uwuArgs = code.replace("uwu ","");
+    var arg1 = uwuArgs.split(' ').slice(0,0)
+    var arg2 = uwuArgs.split(' ').slice(1,1)
+    var lastuwuoutput = uwuArgs.split(' ').slice(2,2)
+    if (arg1 == arg2) {
+      console.log(arg3)
+    }
+  }
+}
