ce01d8ead8e24500e4ca3f1f1b970586cf82d5b3
Samuel Lord <sllord@icloud.com>
2019-11-22 21:09:50 -0600

    Update furcode.js

--- furcode.js
+++ furcode.js
@@ -1,19 +1,3 @@
-var arraysMatch = function (arr1, arr2) {
-
-	// Check if the arrays are the same length
-	if (arr1.length !== arr2.length) return false;
-
-	// Check if all items exist and are in the same order
-	for (var i = 0; i < arr1.length; i++) {
-		if (arr1[i] !== arr2[i]) return false;
-	}
-
-	// Otherwise, return true
-	return true;
-
-};
-
-
 function fureval(code) {
   var cmd = code.split(' ').slice(0,1) 
   if (cmd == "owo") {
@@ -22,10 +6,10 @@ function fureval(code) {
   }
   if (cmd == "uwu") {
     var uwuArgs = code.replace("uwu ","");
-    var arg1 = uwuArgs.split(' ').slice(0,1)
-    var arg2 = var arg2 = uwuArgs.split(' ').slice(1,2)
-    var lastuwuoutput = uwuArgs.split(' ').slice(2);
-    if (arraysMatch(arg1,arg2)) {
+    var arg1 = uwuArgs.split(" ").slice(0,1)
+    var arg2 = uwuArgs.split(" ").slice(1,2)
+    var lastuwuoutput = uwuArgs.split(" ").slice(2);
+    if (arg1.toString() == arg2.toString()) {
       print(lastuwuoutput)
     }
   }
