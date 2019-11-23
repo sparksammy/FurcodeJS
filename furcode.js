45ee694ff1c2f1ebb99b4835d9f6fce437dd34ef
Samuel Lord <sllord@icloud.com>
2019-11-22 21:25:38 -0600

    Extend Functionality

--- furcode.js
+++ furcode.js
@@ -13,4 +13,31 @@ function fureval(code) {
       console.log(lastuwuoutput.toString())
     }
   }
+  if (cmd == "uwuEval") {
+    var uwuArgs = code.replace("uwuEval ","");
+    var arg1 = uwuArgs.split(" ").slice(0,1)
+    var arg2 = uwuArgs.split(" ").slice(1,2)
+    var lastuwuoutput = uwuArgs.split(" ").slice(2);
+    if (arg1.toString() == arg2.toString()) {
+      eval(lastuwuoutput.toString())
+    }
+  }
+    if (cmd == "fursuit") {
+    var uwuArgs = code.replace("fursuit ","");
+    var arg1 = uwuArgs.split(" ").slice(0,1)
+    var arg2 = uwuArgs.split(" ").slice(1,2)
+    var lastuwuoutput = uwuArgs.split(" ").slice(2);
+    if (arg1.toString() != arg2.toString()) {
+      console.log(lastuwuoutput.toString())
+    }
+  }
+  if (cmd == "fursuitEval") {
+    var uwuArgs = code.replace("fursuitEval ","");
+    var arg1 = uwuArgs.split(" ").slice(0,1)
+    var arg2 = uwuArgs.split(" ").slice(1,2)
+    var lastuwuoutput = uwuArgs.split(" ").slice(2);
+    if (arg1.toString() != arg2.toString()) {
+      eval(lastuwuoutput.toString())
+    }
+  }
 }
