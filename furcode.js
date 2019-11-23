ce18ad70fc34bce197ae0f1b414944edef93f461
Samuel Lord <sllord@icloud.com>
2019-11-22 20:36:54 -0600

    Update furcode.js

--- furcode.js
+++ furcode.js
@@ -1,16 +1,16 @@
 
 function fureval(code) {
-  if (code.split(' ').slice(0,1) == "owo") {
-    var owoText = code.replace("owo ","")
-    print(owoText);
+  if (code.includes("owo")) {
+    var owoText = code.replace("owo ","");
+    console.log(owoText)
   }
-  if (code.split(' ').slice(0,1) == "uwu") {
-    var uwuArgs = code.replace("uwu ","")
+  if (code.includes("uwu")) {
+    var uwuArgs = code.replace("uwu ","");
     var arg1 = uwuArgs.split(' ').slice(0,1)
     var arg2 = uwuArgs.split(' ').slice(1,2)
     var lastuwuoutput = uwuArgs.split(' ').slice(2,3)
     if (arg1 == arg2) {
-      print(lastuwuoutput);
+      print(lastuwuoutput)
     }
   }
 }
